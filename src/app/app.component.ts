201f299439ef4e0ab7cab32c5d35d1fb9c21b1ce
chanansiri <nuanpang_c@silpakorn.edu>
2024-09-12 08:09:52 +0700

    fix login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
     private router: Router
   ) { }
   ngOnInit() {
-    alert('????????')
+    // alert('????????')
     // this.liffService.liffLogin();
     liff
       .init({
